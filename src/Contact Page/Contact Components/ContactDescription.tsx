94586a9c10221b25677fb27678a1d6fe01a3031e
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-08 02:52:41 +0000

    error message validation complete

--- src/Contact Page/Contact Components/ContactDescription.tsx
+++ src/Contact Page/Contact Components/ContactDescription.tsx
@@ -1,32 +1,127 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 import "../Contact Components/ContactDescription.css";
 import contactUs from "../../Images/undraw_contact_us.svg";
 import Button from "../../Main Components/Button";
 import warning from "../../Icons/icons8-warning.png";
 
+interface contactState {
+  firstName: string;
+  lastName: string;
+  email: string;
+  issue: string;
+  inputIsNotValid: boolean;
+  message: string;
+}
+
+interface contactAction {
+  type: string;
+  payload: string;
+}
+
+function reducer(state: contactState, action: contactAction): contactState {
+  if (action.type === "firstname") {
+    return {
+      ...state,
+      firstName: action.payload,
+    };
+  }
+  if (action.type === "lastname") {
+    return {
+      ...state,
+      lastName: action.payload,
+    };
+  }
+  if (action.type === "email") {
+    return {
+      ...state,
+      email: action.payload,
+    };
+  }
+  if (action.type === "issue") {
+    return {
+      ...state,
+      issue: action.payload,
+    };
+  }
+  if (action.type === "validName") {
+    return {
+      ...state,
+      inputIsNotValid: true,
+      message: action.payload,
+    };
+  }
+  if (action.type === "validEmail") {
+    return {
+      ...state,
+      inputIsNotValid: true,
+      message: action.payload,
+    };
+  }
+  if (action.type === "validIssue") {
+    return {
+      ...state,
+      inputIsNotValid: true,
+      message: action.payload,
+    };
+  } else {
+    return state;
+  }
+}
 
 function ContactDescription() {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [issue, setIssue] = useState("");
+  const initialState: contactState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    issue: "",
+    inputIsNotValid: false,
+    message: "",
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   function firstnameHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    setFirstName(event.target.value);
+    dispatch({ type: "firstname", payload: event.target.value });
   }
   function lastnameHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    setLastName(event.target.value);
+    dispatch({ type: "lastname", payload: event.target.value });
   }
   function emailHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(event.target.value);
+    dispatch({ type: "email", payload: event.target.value });
   }
   function issueHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setIssue(event.target.value);
+    dispatch({ type: "issue", payload: event.target.value });
   }
 
   function submitContactForm(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
+    if (state.firstName.trim().length < 3) {
+      dispatch({ type: "validName", payload: "Please enter a valid name" });
+      return;
+    }
+    if (!state.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      dispatch({ type: "validEmail", payload: "Please enter a valid email" });
+      return;
+    }
+    if (state.issue.trim().length < 10) {
+      dispatch({ type: "validIssue", payload: "Please type a message" });
+      return;
+    }
+
+    const data = {
+      firtname: state.firstName,
+      lastname: state.lastName,
+      email: state.email,
+      issue: state.issue,
+    };
+
+    console.log(data);
+
+    dispatch({ type: "firstname", payload: "" });
+    dispatch({ type: "lastname", payload: "" });
+    dispatch({ type: "email", payload: "" });
+    dispatch({ type: "issue", payload: "" });
   }
 
   return (
@@ -39,39 +134,39 @@ function ContactDescription() {
         </p>
         <div className="inputs-main">
           <div className="inputs">
-            {/* {errorState.inputIsValid && (
+            {state.inputIsNotValid && (
               <div className="warning-message">
                 <img src={warning} alt="warning" />
-                <span>{errorState.message}</span>
+                <span>{state.message}</span>
               </div>
-            )} */}
+            )}
             <form onSubmit={submitContactForm}>
               <div className="name-inputs">
                 <input
                   placeholder="Enter your firstname"
                   className="firstname"
                   onChange={firstnameHandler}
-                  value={firstname}
+                  value={state.firstName}
                 />
                 <input
                   placeholder="Enter your lastname"
                   className="lastname"
                   onChange={lastnameHandler}
-                  value={lastname}
+                  value={state.lastName}
                 />
               </div>
               <input
                 placeholder="Enter your email"
-                type={"email"}
+                type={"text"}
                 className="email-input"
                 onChange={emailHandler}
-                value={email}
+                value={state.email}
               />
               <textarea
                 placeholder="Describe your issue"
                 className="text-area"
                 onChange={issueHandler}
-                value={issue}
+                value={state.issue}
               />
               <div className="send-button">
                 <Button buttonName="Send" />
