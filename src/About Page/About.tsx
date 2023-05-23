747689b7399be71ba263b957b184f565f3c0f9a8
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-23 14:46:07 +0000

    added login page to other pages

--- src/About Page/About.tsx
+++ src/About Page/About.tsx
@@ -3,10 +3,22 @@ import Navigation from "../Main Components/Navigation";
 import MainWrapper from "../Main Components/MainWrapper";
 import AboutUsDescription from "./About Components/AboutUsDescription";
 import Footer from "../Main Components/Footer";
+import Login from "../Login Page/Login";
+import Modal from "../Main Components/Modal";
+import { useSelector } from "react-redux";
 
 function About() {
+  const loginInitialState = useSelector(
+    (state: any) => state.login.loginIsOpen
+  );
+
   return (
     <>
+      {loginInitialState && (
+        <Modal>
+          <Login />
+        </Modal>
+      )}
       <MainWrapper>
         <Navigation />
         <AboutUsDescription />
--- src/Contact Page/Contact.tsx
+++ src/Contact Page/Contact.tsx
@@ -3,13 +3,25 @@ import Navigation from "../Main Components/Navigation";
 import MainWrapper from "../Main Components/MainWrapper";
 import Footer from "../Main Components/Footer";
 import ContactDescription from "./Contact Components/ContactDescription";
+import Login from "../Login Page/Login";
+import { useSelector } from "react-redux";
+import Modal from "../Main Components/Modal";
 
 function Contact() {
+  const loginInitialState = useSelector(
+    (state: any) => state.login.loginIsOpen
+  );
+
   return (
     <>
+      {loginInitialState && (
+        <Modal>
+          <Login />
+        </Modal>
+      )}
       <MainWrapper>
         <Navigation />
-        <ContactDescription/>
+        <ContactDescription />
       </MainWrapper>
       <Footer />
     </>
