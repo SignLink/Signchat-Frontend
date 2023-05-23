958e78d92d177f30157e5ad494f405fd5e6b88ef
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-23 20:56:59 +0000

    removed unused variables

--- src/Sign Up Page/Signup.tsx
+++ src/Sign Up Page/Signup.tsx
@@ -1,11 +1,8 @@
 import Logo from "./Signup Components/Logo";
 import "../Sign Up Page/Signup.css";
 import SignupInputs from "./Signup Components/SignupInputs";
-import { useSelector } from "react-redux";
 
 function Signup() {
-  const signupState = useSelector((state: any) => state.signup.signupIsOpen);
-  console.log(signupState);
 
   return (
     <div className={`signup-main`}>
