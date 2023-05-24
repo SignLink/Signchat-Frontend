3417e90632afab4fed88603fd31aa30ce3bb236f
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-24 16:58:31 +0000

    added chat

--- src/User Chat Page/User Chat Components/UserChatBox.tsx
+++ src/User Chat Page/User Chat Components/UserChatBox.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function UserChatBox() {
   return (
     <div className="user-chatbox-main">
-      <div className="user-chatbox"></div>
+      <div className="user-chatbox">
+        Chat
+      </div>
       <div className="user-send-message"></div>
     </div>
   );
--- src/User Chat Page/UserChatPage.tsx
+++ src/User Chat Page/UserChatPage.tsx
@@ -1,11 +1,14 @@
 import MainWrapper from "../Main Components/MainWrapper";
 import UserNavigation from "../User Main Components/UserNavigation";
+import UserChatBox from "./User Chat Components/UserChatBox";
+import './UserChatPage.css'
 
 function UserChatPage() {
   return (
     <MainWrapper>
       <div className="user-chat-main">
         <UserNavigation />
+        <UserChatBox/>
       </div>
     </MainWrapper>
   );
