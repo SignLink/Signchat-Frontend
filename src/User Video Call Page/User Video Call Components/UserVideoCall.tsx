46dbf6c1934bc40b64f75861f8b3be71b468cbef
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-24 17:04:53 +0000

    added video call content

---
+++ src/User Video Call Page/User Video Call Components/UserVideoCall.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserVideoCall() {
+  return <div className="user-videocall">Video Call</div>;
+}
+
+export default UserVideoCall;
--- src/User Video Call Page/UserVideoCallPage.tsx
+++ src/User Video Call Page/UserVideoCallPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import MainWrapper from "../Main Components/MainWrapper";
 import UserNavigation from "../User Main Components/UserNavigation";
+import UserVideoCall from "./User Video Call Components/UserVideoCall";
+import './UserVideoCallPage.css'
 
 function UserVideoCallPage() {
   return (
     <MainWrapper>
       <div className="user-videocall-main">
         <UserNavigation />
+        <UserVideoCall/>
       </div>
     </MainWrapper>
   );
