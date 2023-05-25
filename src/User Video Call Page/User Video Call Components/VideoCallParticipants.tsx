4c3be43a047878647cecf2e535a6c9487b389a36
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-05-25 19:15:37 +0000

    added scrolling style

--- src/User Video Call Page/User Video Call Components/VideoCallParticipants.tsx
+++ src/User Video Call Page/User Video Call Components/VideoCallParticipants.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./VideoCallParticipants.css";
 import line from "../../Images/Line.svg";
+import profile from "../../Icons/male-user.svg";
+import mute from "../../Icons/mute-unmute.svg";
 
 function VideoCallParticipants() {
+  const participantsLists = [
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+    {
+      name: "Deborah Smith",
+    },
+  ];
   return (
     <div className="videocall-participants-main">
       <div className="participants">
@@ -10,7 +47,22 @@ function VideoCallParticipants() {
           <h2>Participants</h2>
           <button>View All</button>
         </div>
-        <div className="participants-lists"></div>
+        <div className="participants-lists">
+          {participantsLists.map(function (names, index) {
+            return (
+              <div key={index}>
+                <img src={profile} alt="profile-pic" />
+                <div>
+                  <span>{names.name}</span>
+                  <button>
+                    <img src={mute} alt="mute-button" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+          <div></div>
+        </div>
       </div>
       <span className="line">
         <img src={line} alt="line" />
