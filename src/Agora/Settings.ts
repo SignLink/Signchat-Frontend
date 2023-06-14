2ebb524753c361f585422294212e25b414a83763
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-06-14 19:25:48 +0000

    fixed width and height of image

--- src/Agora/Settings.ts
+++ src/Agora/Settings.ts
@@ -3,6 +3,6 @@ import AgoraRTC from "agora-rtc-sdk-ng";
 //Agora Video Settings
 export const appId = "fc72deb92dd544adb7ed2300bbb52a1a";
 export const token =
-  "007eJxTYChsfLaLiWtzxHwtwY8b1QJj3ldWvu0SLpyd82u6c/xj7hMKDGnJ5kYpqUmWRikppiYmiSlJ5qkpRsYGBklJSaZGiYaJrMltKQ2BjAwC6xsZGRkgEMTnYCjOTM9LzkgsYWAAAOjoITI=";
+  "007eJxTYLg956R92XPxiT1bW+LWX50ve3HheQu7NtaEp1oCH22Wrc5VYEhLNjdKSU2yNEpJMTUxSUxJMk9NMTI2MEhKSjI1SjRM/DmnI6UhkJHB/8MRVkYGCATxORiKM9PzkjMSSxgYAIV6I/c=";
 export const channelName = "signchat";
-export const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+export const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
