16cf28bad0227d132dafaa2d3c4484abb277e50c
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-07-05 00:13:58 +0000

    added content to add friends popup page

--- src/User Friends Page/User Friends Components/AddFriendsPopup.tsx
+++ src/User Friends Page/User Friends Components/AddFriendsPopup.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { JSXElementConstructor, ReactElement, useState } from "react";
 import "./AddFriendsPopup.css";
+import profilePic from "../../Images/christina-morillo-profile.svg";
 
 function AddFriendsPopup() {
+  const friendsPopup = [
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+    {
+      friendName: "Debbie Anderson",
+    },
+  ];
+  const [searchedPeople, setSetPeople] = useState(friendsPopup);
+
+  let searchFriendslists: ReactElement;
+  if (searchedPeople.length === 0) {
+    searchFriendslists = (
+      <div className="empty-message">
+        <span>No Person Found</span>
+      </div>
+    );
+  } else {
+    searchFriendslists = (
+      <>
+        {searchedPeople.map(function (searchedFriends, index) {
+          return (
+            <div className="searched-friend-requests-lists" key={index}>
+              <div className="searched-friends-profile-pic">
+                <img src={profilePic} alt="profile-pic" />
+              </div>
+              <div className="searched-friend-requests-descriptions">
+                <span>{`${searchedFriends.friendName} send you a friend request`}</span>
+                <div className="searched-friend-request-buttons">
+                  <button>Send Friend Request</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
   return (
     <div className="add-friends-main">
       <div className="add-friends-header">
@@ -10,6 +67,7 @@ function AddFriendsPopup() {
       <div className="add-friend-search">
         <input type={"search"} placeholder="Search..." />
       </div>
+      <div className="searched-friends">{searchFriendslists}</div>
     </div>
   );
 }
