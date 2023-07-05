616f59f88f855b83f177204f2c6058afe27161ac
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-07-05 01:57:26 +0000

    added width for user setttings

--- src/User Settings Page/User Settings Components/UserSettings.tsx
+++ src/User Settings Page/User Settings Components/UserSettings.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import UserSettingsProfile from "./UserSettingsProfile";
+import './UserSettings.css'
 
 function UserSettings() {
   return (
-    <div className="user-settings">
+    <div className="user-settings-in-settings-page">
       <UserSettingsProfile />
     </div>
   );
