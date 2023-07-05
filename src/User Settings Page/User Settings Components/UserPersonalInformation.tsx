c75f27012cb41599492ae1da482b89e4aeb246cb
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-07-05 02:58:07 +0000

    added inputs for personal information

---
+++ src/User Settings Page/User Settings Components/UserPersonalInformation.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import editIcon from "../../Icons/edit-30.png";
+import "./UserPersonalInformation.css";
+
+function UserPersonalInformation() {
+  return (
+    <div className="user-personal-information-main">
+      <span className="user-personal-info-title">Personal Information</span>
+      <div className="user-personal-information">
+        <div className="user-personal-information-inputs">
+          <div className="user-personal-information-inputs-1">
+            <div className="personal-info-firstname">
+              <span>Firstname</span>
+              <input type="text" placeholder="Deborah"/>
+            </div>
+            <div className="personal-info-lastname">
+              <span>lastname</span>
+              <input type="text" placeholder="Smith" />
+            </div>
+            <div className="personal-info-sex">
+              {" "}
+              <span>Sex</span>
+              <input type="text" placeholder="Female"/>
+            </div>
+          </div>
+          <div className="user-personal-information-inputs-2">
+            {" "}
+            <span className="user-settings-email">Email</span>
+            <input type="text" placeholder="deborahsmith@yahoo.com" />
+          </div>
+        </div>
+        <div className="settings-edit-button">
+          <button>
+            Edit <img src={editIcon} alt="edit-icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPersonalInformation;
--- src/User Settings Page/User Settings Components/UserSettings.tsx
+++ src/User Settings Page/User Settings Components/UserSettings.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UserSettingsProfile from "./UserSettingsProfile";
 import './UserSettings.css'
+import UserPersonalInformation from "./UserPersonalInformation";
 
 function UserSettings() {
   return (
     <div className="user-settings-in-settings-page">
       <UserSettingsProfile />
+      <UserPersonalInformation/>
     </div>
   );
 }
