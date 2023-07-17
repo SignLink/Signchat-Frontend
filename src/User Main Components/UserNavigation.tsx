051436b3592f9973fa88a72be096ade1f1e0911e
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2023-07-17 15:04:43 +0000

    changed size of video call

--- src/User Main Components/UserNavigation.tsx
+++ src/User Main Components/UserNavigation.tsx
@@ -23,7 +23,7 @@ function UserNavigation() {
       </div>
       <div className="user-navigation-buttons">
         <div className="user-chat">
-          <NavLink to="/user/chat">
+          <NavLink to="/chat">
             <button>
               <img src={chat} alt="chat-icon" />
               <span>Chat</span>
@@ -31,7 +31,7 @@ function UserNavigation() {
           </NavLink>
         </div>
         <div className="user-videocall">
-          <NavLink to="/user/videocall">
+          <NavLink to="/videocall">
             <button>
               <img src={videocall} alt="chat-icon" />
               <span>Video Call</span>
@@ -39,7 +39,7 @@ function UserNavigation() {
           </NavLink>
         </div>
         <div className="user-schedule">
-          <NavLink to="/user/schedule">
+          <NavLink to="/schedule">
             <button>
               <img src={schedule} alt="chat-icon" />
               <span>Schedule</span>
@@ -56,7 +56,7 @@ function UserNavigation() {
         </div>
       </div>
       <div className="user-settings">
-        <NavLink to="/user/settings">
+        <NavLink to="/settings">
           <button>
             <img src={settings} alt="chat-icon" />
             <span>Settings</span>
